a36754c07aa3fb301e0140c512b98324622038e5
Yichen Lu <work@yichen.lu>
2019-10-06 21:26:29 +0800

    get current local time, zoned time, all time zones

--- src/main/java/com/example/demo/api/TimeGetterController.java
+++ src/main/java/com/example/demo/api/TimeGetterController.java
@@ -1,9 +1,35 @@
 package com.example.demo.api;
 
+import com.example.demo.service.TimeService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("api/v1/person")
+import java.util.Set;
+
+@RequestMapping("api/v1/time")
 @RestController
 public class TimeGetterController {
+
+    private final TimeService timeService;
+
+    public TimeGetterController(TimeService timeService) {
+        this.timeService = timeService;
+    }
+
+    @GetMapping(path = "/now")
+    public String getCurrentLocalDateTime(){
+        return timeService.getCurrentDateTime();
+    }
+
+    @GetMapping(path = "/zones")
+    public Set<String> getTimeZones(){
+        return timeService.getAllTimeZone();
+    }
+
+    @GetMapping(path = "/zone")
+    public String getZonedDateTime(@RequestParam(name = "zoneid") String zoneStr){
+        return timeService.getZonedDateTime(zoneStr);
+    }
 }
--- src/main/java/com/example/demo/service/TimeService.java
+++ src/main/java/com/example/demo/service/TimeService.java
@@ -3,11 +3,30 @@ package com.example.demo.service;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+import java.util.Set;
 
 @Service
 public class TimeService {
 
-    public String getCurrentTime(){
-        return LocalDateTime.now().toString();
+    public String getCurrentDateTime(){
+        return LocalDateTime.now().format(
+                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+                .withLocale(Locale.UK));
+    }
+
+    public Set<String> getAllTimeZone(){
+        return ZoneId.getAvailableZoneIds();
+    }
+
+    public String getZonedDateTime(String zoneStr){
+        ZoneId zoneId = ZoneId.of(zoneStr);
+        return ZonedDateTime.now(zoneId).format(
+                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+                .withLocale(Locale.UK));
     }
 }
