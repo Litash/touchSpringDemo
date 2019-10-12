960775926a9fc6337f08b34df28e9d8b4c4de6ad
Yichen Lu <work@yichen.lu>
2019-10-13 00:51:13 +0800

    test time value and format

--- src/test/java/com/example/demo/TimeControllerTest.java
+++ src/test/java/com/example/demo/TimeControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,17 +8,33 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class TimeServiceTest {
+public class TimeControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello World")));
+    public void shouldReturnCurrentTime() throws Exception {
+        String currentDT = LocalDateTime.now().format(
+                DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+                .withLocale(Locale.UK));
+
+        this.mockMvc.perform(get("/api/v1/time/now")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content()
+                        .string(currentDT));
     }
 }
